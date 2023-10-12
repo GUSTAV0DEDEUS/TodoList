d9aabf10adf4fbd0f7619b29a1471fcc480a338a
GUSTAV0DEDEUS <gustavodedeusconceicao@gmail.com>
2023-10-12 18:06:52 -0300

    :construction: development

--- src/main/java/br/com/gustavodedeus/todolist/task/TaskModel.java
+++ src/main/java/br/com/gustavodedeus/todolist/task/TaskModel.java
@@ -1,29 +1,25 @@
-package br.com.gustavodedeus.todolist.user;
+package br.com.gustavodedeus.todolist.task;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
-import lombok.Generated;
-import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
-
 @Data
-@Entity(name = "tb_users")
-public class UserModel {
-
+@Entity(name = "tb_task")
+public class TaskModel {
     @Id
     @GeneratedValue(generator = "UUID")
     private UUID id;
-
-    @Column(unique = true)
-    private String username;
-    private String name;
-    private String password;
-
-
-    @CreationTimestamp
+    @Column(length = 50)
+    private String title;
+    private String description;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
     private LocalDateTime createdAt;
+    private UUID idUser;
 }
