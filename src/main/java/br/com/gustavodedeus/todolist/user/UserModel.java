35323ee4d6d8b65b6b7ef9079f7bd30602cdb3b3
GUSTAV0DEDEUS <gustavodedeusconceicao@gmail.com>
2023-10-12 15:50:24 -0300

    :tada: initial commit

---
+++ src/main/java/br/com/gustavodedeus/todolist/user/UserModel.java
@@ -0,0 +1,10 @@
+package br.com.gustavodedeus.todolist.user;
+import lombok.Data;
+
+@Data
+public class UserModel {
+    private String username;
+    private String name;
+    private String password;
+
+}
